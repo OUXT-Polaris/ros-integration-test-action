cf624845745b31090f0a3f71a2d4bf77ce1849ac
Masaya Kataoka <ms.kataoka@gmail.com>
2021-08-01 01:11:56 +0900

    change into typescript
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ runtime_image/upload_artifact/upload_artifact.ts
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const artifact = require('@actions/artifact');
+const artifactClient = artifact.create()
+const artifactName = 'scenario_test_artifacts';
+const rootDirectory = '/artifacts'
+
+import { dir } from "console";
+import {readdirSync} from "fs"
+    
+export function getFileList(dirPath:string):string[] {
+  let dirList: string[] = new Array();
+  dirList = readdirSync(dirPath, {
+      withFileTypes: true, 
+  }).filter(dirent => dirent.isFile())
+  .map(dirent => dirent.name);
+  return dirList;
+}
+
+export function getDirectoryList(dirPath:string):string[] {
+  let dirList: string[] = new Array();
+  dirList = readdirSync(dirPath, {
+      withFileTypes: true, 
+  }).filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name);
+  return dirList;
+}
+
+const dirs = getDirectoryList(rootDirectory);
+const artifacts = Array<String>();
+dirs.forEach(directory => {
+  const files = getFileList(directory);
+  files.forEach(file => {
+    artifacts.push(file);
+    console.log(file);
+  });
+});
+
+/*
+const options = {
+  continueOnError: false
+}
+
+async function run(): Promise<void> {
+  const uploadResult = await artifactClient.uploadArtifact(artifactName, artifacts, rootDirectory, options)
+}
+
+run()
+*/
