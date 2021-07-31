8b368b1d29c61008d0e69870e940e2defbf773f2
Masaya Kataoka <ms.kataoka@gmail.com>
2021-07-31 11:44:55 +0900

    add --build-arg
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,8 @@ cd /runtime_image
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t runtime_image --build-arg base_image="$BASE_IMAGE" tag="$tag" . && docker run runtime_image
+docker build -t runtime_image \
+    --build-arg base_image="$BASE_IMAGE" \
+    --build-arg tag="$TAG" \
+    . \
+    && docker run runtime_image
