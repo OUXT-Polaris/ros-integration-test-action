c642f43f8004153b525a30d998ca4ddd07ffbf98
Masaya Kataoka <ms.kataoka@gmail.com>
2021-07-31 11:28:28 +0900

    add dockerfile and entrypoints
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM alpine:latest
+
+COPY runtime_image /runtime_image
+COPY entrypoint.sh /entrypoint.sh
+
+RUN apk add --update --no-cache docker
+RUN ["chmod", "+x", "/entrypoint.sh"]
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+
+#!/bin/sh -l
+
+BASE_IMAGE=$1
+TAG=$2
+
+cd /runtime_image
+
+# here we can make the construction of the image as customizable as we need
+# and if we need parameterizable values it is a matter of sending them as inputs
+docker build -t runtime_image --build-arg base_image="$BASE_IMAGE" tag="$tag" . && docker run runtime_image
