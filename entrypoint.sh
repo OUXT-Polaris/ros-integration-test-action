57914c81c4fdef4a902c894773ce7f10853d2eb2
Masaya Kataoka <ms.kataoka@gmail.com>
2021-08-01 23:26:29 +0900

    specify https
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,8 +28,8 @@ echo "npm install" >> entrypoint.sh
 echo "npm run upload $ARTIFACTS_NAME" >> entrypoint.sh
 
 touch .netrc
-echo "machine github.com" >> .netrc
-echo "login $GITHUB_CLONE_UERNAME" >> .netrc
+echo "machine https://github.com" >> .netrc
+echo "login $GITHUB_CLONE_USERNAME" >> .netrc
 echo "password $GITHUB_CLONE_TOKEN" >> .netrc
 
 # here we can make the construction of the image as customizable as we need
