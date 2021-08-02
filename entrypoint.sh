dde7017aa4927c3676935072d0fd4909a84a3b01
Masaya Kataoka <ms.kataoka@gmail.com>
2021-08-02 22:49:38 +0900

    modify execution order
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,9 +19,6 @@ echo "ACTIONS_RUNTIME_URL=$ACTIONS_RUNTIME_URL" >> entrypoint.sh
 echo "export ACTIONS_RUNTIME_URL" >> entrypoint.sh
 echo "GITHUB_RUN_ID=$GITHUB_RUN_ID" >> entrypoint.sh
 echo "export GITHUB_RUN_ID" >> entrypoint.sh
-echo "sh /opt/ros/$ROSDISTRO/setup.sh" >> entrypoint.sh
-echo "$TEST_COMMAND > /artifacts/test_command_output.txt" >> entrypoint.sh
-echo "$CHECK_RESULT_COMMAND > /artifacts/check_result_command.txt" >> entrypoint.sh
 echo "cd /artifact_controller" >> entrypoint.sh
 echo "npm install" >> entrypoint.sh
 echo "npm run download $REPOS_ARTIFACTS_NAME" >> entrypoint.sh
@@ -29,9 +26,10 @@ echo "cd /" >> entrypoint.sh
 echo "find $REPOS_FILENAME" >> entrypoint.sh
 echo "cd /colcon_ws" >> entrypoint.sh
 echo "vcs import src < /$REPOS_FILENAME" >> entrypoint.sh
-echo "rosdep install -iry --from-paths src --rosdistro $ROSDISTRO" >> entrypoint.sh
-echo "colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release" >> entrypoint.sh
-echo "sh /colcon_ws/install/local_setup.sh" >> entrypoint.sh
+echo "sh /opt/ros/$ROSDISTRO/setup.sh && rosdep install -iry --from-paths src --rosdistro $ROSDISTRO" >> entrypoint.sh
+echo "sh /opt/ros/$ROSDISTRO/setup.sh && colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release" >> entrypoint.sh
+echo "sh /opt/ros/$ROSDISTRO/setup.sh && sh /colcon_ws/install/local_setup.sh && $TEST_COMMAND > /artifacts/test_command_output.txt" >> entrypoint.sh
+echo "sh /opt/ros/$ROSDISTRO/setup.sh && sh /colcon_ws/install/local_setup.sh && $CHECK_RESULT_COMMAND > /artifacts/check_result_command.txt" >> entrypoint.sh
 echo "cd /artifact_controller" >> entrypoint.sh
 echo "npm run upload $ARTIFACTS_NAME" >> entrypoint.sh
 
